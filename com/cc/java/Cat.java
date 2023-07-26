1954c4d71bfbf946cc93a840e9eb618e5498b7ca
Rekxis <Rekxis@gmx.de>
2023-07-26 12:23:44 +0200

    cat? cat

---
+++ com/cc/java/Cat.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Cat {
+
+    // this --> Instanzvariable
+
+    public void tellYourAdress() {
+        System.out.println("Blick von innen: " + this);
+    }
+
+}
