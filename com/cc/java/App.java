e43c516d3535b9ca58c70cf6e0cbebcd6e531f75
Rekxis <Rekxis@gmx.de>
2023-07-27 10:44:06 +0200

    Typkonvertierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,19 +4,33 @@ public class App {
 
     public static void main(String[] args) {
 
-        /* Instanziierung Klasse --> Objekt */
-        // Cat -- Referenztyp
-        // cat -- Referenzvariable
-
         Cat cat1 = new Cat();
-        output("Blick von aussen:" + cat1);
-        output(cat1.tellYourAdress());
 
+        // Wertzuweisung
+        cat1.firstName = "Grizabella";
+        cat1.furColor = "white";
+        cat1.age = 29;
+
+        output(cat1.firstName);
+        output(cat1.furColor);
+
+        // .. was nicht passt, wird passend gemacht..
+        // .. Typkonvertierung
+
+        output(Integer.toString(cat1.age));
+        output(String.valueOf(cat1.age));
+        
         output("-------------------");
 
         Cat cat2 = new Cat();
-        output("Blick von aussen:" + cat2);
-        output(cat2.tellYourAdress());
+
+        cat2.firstName = "Alonso";
+        cat2.furColor = "grey";
+        cat2.age = 35;
+
+        output(cat2.firstName);
+        output(cat2.furColor);
+        output(String.valueOf(cat2.age));
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,14 +2,8 @@ package com.cc.java;
 
 public class Cat {
 
-    // this --> Instanzvariable
-
-    // public void tellYourAdress() {
-    // System.out.println("Blick von innen: " + this);
-    // }
-
-    public String tellYourAdress() {
-        return "Blick von innen: " + this;
-    }
+    public String firstName;
+    public String furColor;
+    int age;
 
 }
